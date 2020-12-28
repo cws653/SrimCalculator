15efd60c62f08dfe02031f6f33df36cc05890bb6
최원석 <cws653@naver.com>
2020-12-28 23:21:05 +0900

    Charts 프레임워크를 이용하여 LineGraph 포맷 만듬

--- SrimCalculator/SrimCalculator/ChartsViewController.swift
+++ SrimCalculator/SrimCalculator/ChartsViewController.swift
@@ -11,44 +11,62 @@ import Charts
 
 class ChartsViewController: UIViewController {
     
-    lazy var chartsView = ChartsView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height))
+    @IBOutlet weak var lineGraphView: LineChartView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        chartsView.setUp()
-        view.addSubview(chartsView)
+        let months: [String] = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        let unitsSold: [Double] = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0, 2.0, 4.0, 5.0, 4.0]
+        
+        lineGraphView.noDataText = "데이터가 없습니다."
+        lineGraphView.noDataFont = .systemFont(ofSize: 20)
+        lineGraphView.noDataTextColor = .lightGray
+        
+        setChart(dataPoints: months, values: unitsSold)
+       
+        
+        
+//        for i in 0..<dataPoint.count {
+//            let dataEntry = ChartDataEntry(x: Double(i), y: Double(dataArray[i]))
+//            dataEntries.append(dataEntry)
+//        }
+//
+//        let line1 = LineChartDataSet(entries: dataEntries, label: "Numbers")
+//        line1.colors = [NSUIColor.blue]
+//
+//        let data = LineChartData()
+//        data.addDataSet(line1)
+////        let chartDataSet = ChartDataSet(entries: dataEntries, label: "Numbers")
+////        let chartData = ChartData(dataSet: chartDataSet)
+//
+//        lineGraphView.data = data
     }
-}
-
-class ChartsView: UIView {
-    var chart: LineChartView!
-    var dataSet: LineChartDataSet!
     
-    func setUp()
-    {
-        self.backgroundColor = .red
-        // Sample data
-        let values: [Double] = [8, 104, 81, 93, 52, 44, 97, 101, 75, 28,
-            76, 25, 20, 13, 52, 44, 57, 23, 45, 91,
-            99, 14, 84, 48, 40, 71, 106, 41, 45, 61]
+    private func setChart(dataPoints: [String], values: [Double]) {
+        var lineChartEntry = [ChartDataEntry]()
         
-        var entries: [ChartDataEntry] = Array()
-        
-        for (i, value) in values.enumerated()
-        {
-            entries.append(ChartDataEntry(x: Double(i), y: value, icon: UIImage(named: "icon", in: Bundle(for: self.classForCoder), compatibleWith: nil)))
+        for i in 0..<dataPoints.count {
+            let dataEntry = ChartDataEntry(x: Double(i), y: values[i])
+            lineChartEntry.append(dataEntry)
         }
         
-        dataSet = LineChartDataSet(entries: entries, label: "First unit test data")
-        dataSet.drawIconsEnabled = false
-        dataSet.iconsOffset = CGPoint(x: 0, y: 20.0)
-
-        chart = LineChartView(frame: CGRect(x: 0, y: 0, width: 480, height: 350))
-        chart.backgroundColor = NSUIColor.clear
-        chart.leftAxis.axisMinimum = 0.0
-        chart.rightAxis.axisMinimum = 0.0
-        chart.data = LineChartData(dataSet: dataSet)
-        self.addSubview(chart)
+        let line = LineChartDataSet(entries: lineChartEntry, label: "Numbers")
+        line.colors = [NSUIColor.blue]
+        line.highlightEnabled = false
+        
+        let data = LineChartData(dataSet: line)
+        lineGraphView.data = data
+        
+        lineGraphView.xAxis.labelPosition = .bottom
+        lineGraphView.xAxis.valueFormatter = IndexAxisValueFormatter(values: dataPoints)
+        lineGraphView.xAxis.setLabelCount(dataPoints.count, force: false)
+        lineGraphView.rightAxis.enabled = false
+        lineGraphView.animate(xAxisDuration: 2.0, yAxisDuration: 2.0)
+        
+        lineGraphView.leftAxis.axisMaximum = 30
+        lineGraphView.leftAxis.axisMinimum = -10
     }
+    
 }
+
